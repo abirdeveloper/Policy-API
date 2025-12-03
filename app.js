2bcdf45802fdcfbe39a16316fc09345babcdb17c
abirdeveloper <abirindra@rocketmail.com>
2025-12-03 12:31:51 +0000

    Apply fixes suggested by AI reviewer

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ var cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 
-app.use(cors())
+const trustedOrigins = ['http://localhost:3000', 'https://your-production-domain.com'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || trustedOrigins.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}))
 const port = process.env.PORT || 5005
 const booksDS = require('./lib/policyDS')
 const route = require('./routes/route')
--- controller.js
+++ controller.js
@@ -1,33 +1,68 @@
 const bookAdaptor = require('./lib/policy-adaptor')
 
-const getAll = () => {
-    const books = bookAdaptor.getAll();
-    return books
+const getAll = async (req, res, next) => {
+    try {
+        const books = await bookAdaptor.getAll();
+        res.status(200).json(books);
+    } catch (error) {
+        next(error);
+    }
 }
 
-const getOne = (_id) => {
-    const book = bookAdaptor.getOne(_id);
-    return book
+const getOne = async (req, res, next) => {
+    try {
+        const book = await bookAdaptor.getOne(req.params.id);
+        if (!book) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        res.status(200).json(book);
+    } catch (error) {
+        next(error);
+    }
 }
 
-const add = (book) => {
-    const books = bookAdaptor.add(book)
-    return books
-}
-const addNewBook = (book)=>{
-  return  bookAdaptor.addNewBook(book)
-}
-const updateNewBook = (book)=>{
-    return  bookAdaptor.updateBook(book)
-  }
-
-const deleteBookByCode = (code)=>{
-    return bookAdaptor.deleteNewBook(code)
+const addNewBook = async (req, res, next) => {
+    try {
+        const newBook = await bookAdaptor.addNewBook(req.body);
+        res.status(201).json(newBook);
+    } catch (error) {
+        next(error);
+    }
 }
 
-const searchBookname = async(policynumber)=>{
-    return await bookAdaptor.searchNewBook(policynumber)
+const updateNewBook = async (req, res, next) => {
+    try {
+        const updatedBook = await bookAdaptor.updateBook(req.body);
+        if (!updatedBook) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        res.status(200).json(updatedBook);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const deleteBookByCode = async (req, res, next) => {
+    try {
+        const result = await bookAdaptor.deleteNewBook(req.params.code);
+        if (!result) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        res.status(200).json({ message: 'Book deleted successfully' });
+    } catch (error) {
+        next(error);
+    }
+}
+
+const searchBookname = async (req, res, next) => {
+    try {
+        const policynumber = req.query.policynumber;
+        const books = await bookAdaptor.searchNewBook(policynumber);
+        res.status(200).json(books);
+    } catch (error) {
+        next(error);
+    }
 }
 
 
-module.exports = {getAll, getOne, addNewBook,deleteBookByCode,updateNewBook,searchBookname}
+module.exports = { getAll, getOne, addNewBook, deleteBookByCode, updateNewBook, searchBookname }
